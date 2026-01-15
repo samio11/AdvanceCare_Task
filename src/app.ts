f30683f0c5a3c84cb66ee5d07acf4c895084b213
samio11 <samiohasan6@gmail.com>
2026-01-15 20:28:11 +0600

    Error Handler done and configure cloudinary for image upload

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { globalErrorHandler } from "./app/middlewares/GlobalErrorHandler";
+import { notFound } from "./app/middlewares/notFound";
+import cookieParser from "cookie-parser";
 const app: Application = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
@@ -11,5 +15,8 @@ app.get("/", (req: Request, res: Response) => {
     Date: new Date().toISOString(),
   });
 });
+
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
---
+++ src/app/middlewares/GlobalErrorHandler.ts
@@ -0,0 +1,78 @@
+import { NextFunction, Request, Response } from "express";
+import { handleCastError } from "../errors/handleCastError";
+import { handleDuplicateError } from "../errors/handleDuplicateError";
+import { handleValidationError } from "../errors/handleValidationError";
+import { handleZodError } from "../errors/handleZodError";
+import { AppError } from "../errors/AppError";
+import config from "../config";
+import { TErrorSources } from "../errors/error.type";
+import { deleteImageFromCloudinary } from "../config/cloudinary.config";
+
+export const globalErrorHandler = async (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req?.file) {
+    await deleteImageFromCloudinary(req?.file?.path);
+  }
+  if (req?.files && req?.files?.length) {
+    const excludePath = (req?.files as Express.Multer.File[]).map(
+      (x) => x.path
+    );
+    await Promise.all(excludePath.map((x) => deleteImageFromCloudinary(x)));
+  }
+  let message = "Something went wrong";
+  let statusCode = 500;
+  let errorSources: TErrorSources = [
+    {
+      path: "",
+      message: "Opps! Error",
+    },
+  ];
+  if (err.code === 11000) {
+    const x = handleDuplicateError(err);
+    statusCode = x.statusCode;
+    message = x.message;
+    errorSources = x.errorSources;
+  } else if (err.name === "CastError") {
+    const x = handleCastError(err);
+    statusCode = x.statusCode;
+    message = x.message;
+    errorSources = x.errorSources;
+  } else if (err.name === "ValidationError") {
+    const x = handleValidationError(err);
+    statusCode = x.statusCode;
+    message = x.message;
+    errorSources = x.errorSources;
+  } else if (err.name === "ZodError") {
+    const x = handleZodError(err);
+    statusCode = x.statusCode;
+    message = x.message;
+    errorSources = x.errorSources;
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorSources = [
+      {
+        path: "",
+        message: err?.message,
+      },
+    ];
+  } else if (err instanceof Error) {
+    message = err?.message;
+    errorSources = [
+      {
+        path: "",
+        message: err?.message,
+      },
+    ];
+  }
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorSources,
+    stack: config.NODE_ENV === "development" ? err?.stack : "",
+  });
+};
